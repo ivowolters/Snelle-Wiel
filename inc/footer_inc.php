9ae09db3356c996a9c23e2ecb2b2998bb418e7c6
Ivo <ivowo2@gmail.com>
2017-11-21 14:00:07 +0100

    New version

--- inc/footer_inc.php
+++ inc/footer_inc.php
@@ -1,10 +1,3 @@
-        </div>
-
-    <!-- Bootstrap core JavaScript -->
-    <script src="<?php echo BaseClass::$oVariables->sTemplateFolder; ?>jquery/jquery.min.js"></script>
-    <script src="<?php echo BaseClass::$oVariables->sTemplateFolder; ?>js/bootstrap.min.js"></script>
-    <div class="card-footer">
-        <input type="button" value="Uitloggen" onclick="window.location.href = '<?php echo BaseClass::$oVariables->sDomainOnly; ?>uitloggen'">
-    </div>
-    </body>
-</html>
+<div class="card-footer">
+    <input type="button" value="Uitloggen" onclick="window.location.href = '<?php echo BaseClass::$oVariables->sDomainOnly; ?>uitloggen'">
+</div>
--- template-2017/inc/end-inc.php
+++
@@ -1,8 +0,0 @@
-
-<!-- Bootstrap core JavaScript -->
-<script src="<?php echo BaseClass::$oVariables->sTemplateFolder; ?>jquery/jquery.min.js"></script>
-<script src="<?php echo BaseClass::$oVariables->sTemplateFolder; ?>popper/popper.min.js"></script>
-<script src="<?php echo BaseClass::$oVariables->sTemplateFolder; ?>js/bootstrap.min.js"></script>
-
-</body>
-</html>
